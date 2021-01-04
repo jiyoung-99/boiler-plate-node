92f45b3181a498f534e41a0dd6406040b166dc0a
jiyoung-99 <1skylar1111@gmail.com>
2021-01-04 21:57:37 +0900

    /regist, decrypt

--- index.js
+++ index.js
@@ -10,16 +10,15 @@ app.use(bodyParser.urlencoded({extened:true}));
 //application/json
 app.use(bodyParser.json());
 
-
-
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://@boiler-plate-node.inyzj.mongodb.net/boiler-plate-node?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://dbUser:dbUserPassword@boiler-plate-node.inyzj.mongodb.net/boiler-plate-node?retryWrites=true&w=majority', {
     useNewUrlParser: true, useUnifiedTopology:true, useCreateIndex: true, useFindAndModify: false
 }).then(()=>console.log('MongoDB Connected....'))
   .catch(err=>console.log(err))
 
 app.get('/', (req, res) => res.send('새해복많이받으세요'))
 
+//회원가입
 app.post('/register', (req, res)=> {
   //회원 가입 할 때 필요한 정보들을 client에서 가져오면
   //해당 정보를 database에 넣어준다.
@@ -27,11 +26,29 @@ app.post('/register', (req, res)=> {
   const user = new User(req.body)
 
   user.save((err, userInfo) => {
-    if(err) return res.json({success: false, err})
+    if(err) {
+      console.log('index.js is err : ', err)
+
+      return res.json({success: false, err})
+    }
+    
+      
     return res.status(200).json({
       success: true
     })
   })
+
+})
+
+//로그인
+app.post('/login', (req, res) => {
+
+  //요청된 이메일이 데이터베이스에 있는지 확인
+
+  //요청된 이메일이 데이터 베이스에 있다면 비밀번호가 맞는 비밀번호인지 확인
+
+  //비밀번호까지 맞다면 토큰 생성
+
 
 })
 
--- models/User.js
+++ models/User.js
@@ -1,4 +1,9 @@
 const mongoose = require('mongoose');
+//bcrypt 가져온다.
+const bcrypt = require('bcrypt')
+//salt를 이용해서 비밀번호를 암호화, salt를 먼저 생성해야한다. 10자리인 salt를 이용해서 비밀번호를 암호화한다.
+const saltRounds = 10
+
 
 const userSchema = mongoose.Schema({
     name: {
@@ -30,6 +35,33 @@ const userSchema = mongoose.Schema({
         type: Number
     }
 })
+
+//userModel에 유저 정보를 저장하기 전에 'save'를 하겠다 라는 뜻
+userSchema.pre('save', function( next ) {
+
+    var user = this;
+    
+    if(user.isModified('password')){
+
+        //비밀번호를 암호화 시킨다.
+        bcrypt.genSalt(saltRounds, function(err, salt) {
+    
+    
+            if(err) return next(err)
+    
+            bcrypt.hash(user.password, salt, function(err, hash) {
+                // Store hash in your password DB.
+                if(err) return next(err)
+                user.password = hash
+                next()
+                
+            });
+    
+        });
+    }
+
+})
+
 
 const User = mongoose.model('User', userSchema)
 
