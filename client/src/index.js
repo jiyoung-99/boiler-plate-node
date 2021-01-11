e65aee6053f4b42baebbe4afa13e05e06472fada
jiyoung-99 <1skylar1111@gmail.com>
2021-01-11 16:51:31 +0900

    antd 디자인

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// antd 디자인
+import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
